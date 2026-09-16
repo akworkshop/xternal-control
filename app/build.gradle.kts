b1c2d9ae7f8e2f3e2241d9962c1917dcd8aa191a
arkay107 <arkay107@gmail.com>
2026-09-16 19:58:36 +1000

    feat: release v1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xternal.control"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.2.2"
+        versionCode = 20
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
