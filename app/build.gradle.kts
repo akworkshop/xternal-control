2906ecf8bb75e90b526b8ea4c43e0f60ce34ee72
arkay107 <arkay107@gmail.com>
2026-09-22 18:08:41 +1000

    Release v1.2.8: Add multi-display resizeableActivity manifest attributes for external displays

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xternal.control"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.2.7"
+        versionCode = 25
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
